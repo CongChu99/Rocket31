b8b29742f99ad2e3bb8ea645381d8e97b0d68706
Cong Chu <92419576+CongChu99@users.noreply.github.com>
2023-03-08 19:46:47 +0700

    update

--- database/lesson_04.sql
+++ database/lesson_04.sql
@@ -25,6 +25,17 @@ JOIN department USING (department_id)
 GROUP BY department_id
 HAVING total_accounts > 3;
 
+-- Question 5
+SELECT question.*
+FROM question
+JOIN exam_question USING (question_id)
+GROUP BY question_id
+HAVING COUNT(exam_id) = (SELECT MAX(total_exams)
+						FROM 	(SELECT COUNT(exam_id) AS total_exams
+								FROM question
+								JOIN exam_question USING (question_id)
+								GROUP BY question_id) AS t1);
+
 -- Question 6
 SELECT cq.*, COUNT(question_id)
 FROM category_question AS cq
@@ -33,24 +44,40 @@ GROUP BY category_id;
 
 
 -- Question 7 ( ve chua )
-SELECT exam_question.*, COUNT(question_id)
+SELECT question.*, COUNT(exam_id) AS total_exams
 FROM question
 LEFT JOIN exam_question USING(question_id)
-GROUP BY exam_id;
+GROUP BY question_id;
+
+-- Question 8
+SELECT question.*
+FROM answer
+JOIN question USING(question_id)
+GROUP BY question_id
+HAVING COUNT(answer_id) =  (SELECT MAX(total_answers)
+							FROM (SELECT COUNT(answer_id) AS total_answers
+									FROM answer
+									GROUP BY question_id) AS t1);
 
 -- Question 9 (ve chua)
-SELECT *
-FROM `account`
-LEFT JOIN `group_account` USING (account_id);
+SELECT `group`.*, COUNT(account_id) AS total_accounts
+FROM `group`
+LEFT JOIN `group_account` USING (group_id)
+GROUP BY group_id;
+
+-- Question
 
 -- Question 14,15
 SELECT `group`.*
 FROM `group`
 LEFT JOIN group_account USING (group_id)
-WHERE account_id IS NULL
+WHERE account_id IS NULL;
 
 -- Question 16 ( ve lam)
-
+SELECT question.*
+FROM question
+LEFT JOIN answer USING (question_id)
+WHERE answer_id is NULL;
 
 
 
